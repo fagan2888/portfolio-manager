aa90a7db4e566c854fed5303691fc5f58310d739
Gurchet Rai <gurch101@gmail.com>
2015-12-30 15:45:51 -0800

    add force refresh

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 import decimal
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from utils.formatters import timefmt, moneyfmt, pctfmt
+import config
+import portfolio
 
 app = Flask(__name__)
 app.jinja_env.filters['time'] = timefmt
@@ -11,9 +13,11 @@ app.jinja_env.filters['pct'] = pctfmt
 
 @app.route('/')
 def index():
-    with open('positions.json') as f:
-        positions = json.loads(f.read(), parse_float=decimal.Decimal)
-    print positions['categories']
+    if request.args.get('force_refresh', '') != '':
+        positions = portfolio.init_portfolio(config.POSITIONS_FILE)
+    else:
+        with open('positions.json') as f:
+            positions = json.loads(f.read(), parse_float=decimal.Decimal)
     return render_template('portfolio.html', positions=positions)
 
 
