6f56e2f010d9ddc8c62fd5ac824cb45f3adfa2e4
Gurchet Rai <gurch101@gmail.com>
2016-01-04 23:48:51 -0800

    add authentication

--- config.py
+++ config.py
@@ -6,3 +6,8 @@ MAILGUN_API_URL = ''
 MAILGUN_API_KEY = ''
 EMAIL_FROM = ''
 EMAIL_TO = EMAIL_FROM
+
+# flask settings
+SECRET_KEY = 'mysupersecretkey'
+# password is foo
+PASSWORD = '$pbkdf2-sha256$20000$qbW2NgYgJAQAgNC6l7K2tg$A2CU7gQhxsHRPRgwQ1zpucH7DN/BhGMLL83TU3Cq7ls'
--- server.py
+++ server.py
@@ -1,18 +1,44 @@
 import decimal
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for, abort
+from passlib.hash import pbkdf2_sha256
 from utils.formatters import timefmt, moneyfmt, pctfmt
 import config
 import portfolio
 
 app = Flask(__name__)
+app.config.from_object(config)
 app.jinja_env.filters['time'] = timefmt
 app.jinja_env.filters['money'] = moneyfmt
 app.jinja_env.filters['pct'] = pctfmt
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return '''
+            <form action="" method="post">
+                <input type="text" placeholder="Username" name="username">
+                <input type="password" placeholder="Password" name="password">
+                <input type="submit" value="Login">
+            </form>
+        '''
+    else:
+        print 'here'
+        print app.config
+        uname = request.form['username']
+        passwd = request.form['password']
+        if uname is not None and pbkdf2_sha256.verify(passwd, app.config['PASSWORD']):
+            session['uname'] = uname
+            return redirect(url_for('index'))
+    abort(401)
+
+
 @app.route('/')
 def index():
+    if session.get('uname', None) is None:
+        return redirect(url_for('login'))
+
     if request.args.get('force_refresh', '') != '':
         positions = portfolio.init_portfolio(config.POSITIONS_FILE)
     else:
