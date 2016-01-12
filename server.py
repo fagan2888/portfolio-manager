1c9537a01b1169dabc572924dba60d3ff2f63390
Gurchet Rai <gurch101@gmail.com>
2016-01-12 14:19:34 -0800

    add login decorator

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
-import decimal
-import json
 from shutil import move
 from os import remove
+from functools import wraps
 from flask import (Flask, render_template, request, jsonify,
                    session, redirect, url_for, abort)
 from passlib.hash import pbkdf2_sha256
@@ -14,6 +13,15 @@ app.config.from_object(config)
 app.jinja_env.filters['time'] = timefmt
 app.jinja_env.filters['money'] = moneyfmt
 app.jinja_env.filters['pct'] = pctfmt
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get('uname', None) is None:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -29,7 +37,8 @@ def login():
     else:
         uname = request.form['username']
         passwd = request.form['password']
-        if (uname is not None and pbkdf2_sha256.verify(passwd, app.config['PASSWORD'])):
+        if (uname is not None and
+                pbkdf2_sha256.verify(passwd, app.config['PASSWORD'])):
             session['uname'] = uname
             return redirect(url_for('index'))
     abort(401)
@@ -41,28 +50,29 @@ def logout():
     return redirect(url_for('login'))
 
 
-
 @app.route('/')
+@login_required
 def index():
-    if session.get('uname', None) is None:
-        return redirect(url_for('login'))
-    return render_template('index.html', portfolio=init_portfolio(config.POSITIONS_FILE))
+    return render_template(
+        'index.html',
+        portfolio=init_portfolio(config.POSITIONS_FILE)
+    )
 
 
 @app.route('/positions', methods=['GET'])
+@login_required
 def get_positions():
-    if session.get('uname', None) is None:
-        return redirect(url_for('login'))
     return jsonify(portfolio=init_portfolio(config.POSITIONS_FILE))
 
+
 @app.route('/positions', methods=['POST'])
+@login_required
 def set_positions():
-    if session.get('uname', None) is None:
-        return redirect(url_for('login'))
     new_positions = request.get_json()['positions']
     with open(config.POSITIONS_FILE, 'r') as old_file:
         with open('tmp.txt', 'w') as new_file:
             keys = old_file.readline().strip().split(',')
+            num_keys = len(keys)
             new_file.write(','.join(keys) + '\n')
             for line in old_file:
                 old_position = dict(zip(keys, line.strip().split(',')))
@@ -72,7 +82,7 @@ def set_positions():
                     if old_position['qty'] != 0:
                         for k in keys[:-1]:
                             new_file.write(str(old_position[k]) + ',')
-                        new_file.write(old_position[keys[len(keys) - 1]] + '\n')
+                        new_file.write(old_position[keys[num_keys - 1]] + '\n')
                 else:
                     new_file.write(line + '\n')
     remove(config.POSITIONS_FILE)
