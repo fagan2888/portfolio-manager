b153e70c566e301d320359d4ab9cb6267d5bf89a
Gurchet Rai <gurch101@gmail.com>
2016-01-14 14:51:18 -0800

    simplify fetcher daemon

--- daemons/fetcher.py
+++ daemons/fetcher.py
@@ -1,4 +1,4 @@
-"""Saves a copy of the portfolio every day at 9:00. """
+"""Saves a copy of the portfolio every day at 6:00 PM. """
 
 import os
 import time
@@ -11,22 +11,22 @@ import config
 def save_portfolio():
     """Saves a copy of the portfolio. """
 
+    ymd = time.strftime('%Y-%m-%d', time.localtime(time.time()))
     positions = portfolio.init_portfolio(config.POSITIONS_FILE)
     positions_json = json.dumps(positions, use_decimal=True)
+    positions_filename = ('%s-positions.json' % (ymd, ))
 
-    if os.path.isfile('positions.json'):
-        try:
-            os.makedirs('archive')
-        except OSError:
-            pass
-        os.rename('positions.json',
-                  'archive/positions.json.%s' % (int(time.time()),))
-    with open('positions.json', 'w') as f:
+    try:
+        os.makedirs('archive')
+    except OSError:
+        pass
+
+    with open('archive/' + positions_filename, 'w') as f:
         f.write(positions_json)
 
 
 save_portfolio()
-schedule.every().day.at('9:00').do(save_portfolio)
+schedule.every().day.at('6:00').do(save_portfolio)
 
 while True:
     schedule.run_pending()
