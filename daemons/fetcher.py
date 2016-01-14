be0e3ac8905edd1b9524f6f352c419f41ac4d10d
Gurchet Rai <gurch101@gmail.com>
2016-01-14 15:18:02 -0800

    save daily market values

--- daemons/fetcher.py
+++ daemons/fetcher.py
@@ -18,15 +18,38 @@ def save_portfolio():
 
     try:
         os.makedirs('archive')
+        os.makedirs('stats')
     except OSError:
         pass
 
     with open('archive/' + positions_filename, 'w') as f:
         f.write(positions_json)
 
+    with open('stats/mktval.txt', 'w') as f:
+        f.write(ymd + ',' + str(positions['total']) + '\n')
+
+    with open('stats/catmktval.txt', 'w') as f:
+        for category in positions['categories']:
+            f.write((
+                ymd + ',' +
+                category['name'] + ',' +
+                str(category['mktvalue']) +
+                '\n'
+            ))
+
+    with open('stats/posmktval.txt', 'w') as f:
+        for ticker, position in positions['positions'].iteritems():
+            f.write((
+                ymd + ',' +
+                ticker + ',' +
+                str(position['qty']) + ',' +
+                str(position['mktvalue']) +
+                '\n'
+            ))
+
 
 save_portfolio()
-schedule.every().day.at('6:00').do(save_portfolio)
+schedule.every().day.at('18:00').do(save_portfolio)
 
 while True:
     schedule.run_pending()
